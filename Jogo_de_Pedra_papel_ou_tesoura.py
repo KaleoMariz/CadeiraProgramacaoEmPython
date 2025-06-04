9ea6bfb1864d85937dc34316219aeca2fdb4591f
Igor jr <igorjuniorr12@gmail.com>
2025-06-04 14:31:18 -0300

    Inicio

---
+++ Jogo_de_Pedra_papel_ou_tesoura.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+# cores 
+
+cor1 = #020122 #fundo
+cor2 = #0A0929
+cor3 = #141331
+cor4 = #24233F
+cor5 = #2E2D48 
+cor6 = #FFFFFF #letras
+
